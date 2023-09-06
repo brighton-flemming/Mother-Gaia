2127b89c8a24fb3e873ea307658ec15a8cb7b031
brighton.flemming <brighton.flemming@student.moringaschool.com>
2023-09-06 10:36:08 +0300

    Mission Passed

--- app/models.py
+++ app/models.py
@@ -8,5 +8,17 @@ class User(Base):
     id = Column(Integer, primary_key=True)
     username = Column(String(50), nullable=False, unique=True)
     age = Column(Integer, nullable=False,unique=True)
-    email = Column(String)
+    email = Column(String, nullable=True)
+    bottles = relationship('Bottle', back_populates='user')
+
+class Bottle(Base):
+    __tablename__ = 'bottles'
+
+    id = Column(Integer, primary_key=True)
+    user_id =  Column(Integer, ForeignKey('users.id'), nullable=False)
+    bottles_recycled = Column(Integer, nullable=False, default=0)
+
+    user = relationship('User', back_populates='bottles')
+
+
 
