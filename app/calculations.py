83af8d6321e68006be2db798e7a3b13da05720cd
brighton.flemming <brighton.flemming@student.moringaschool.com>
2023-09-06 20:26:47 +0300

    Mission Passed

--- app/calculations.py
+++ app/calculations.py
@@ -7,3 +7,10 @@ def calculate_tree_statistics(session:Session, user_id: int) -> dict:
     trees_planted = session.query(Tree).filter_by(user_id=user_id, action='planted').count()
     trees_cut_down = session.query(Tree).filter_by(user_id=user_id, action='cut down').count()
     net_effect = trees_planted - trees_cut_down
+    trees_statistics = {
+        'trees_planted': trees_planted,
+        'trees_cut_down': trees_cut_down,
+        'net_effect': net_effect
+    }
+
+    return trees_statistics
