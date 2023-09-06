180b97dbc815a01f91c0cac923503bcb5f7994b1
brighton.flemming <brighton.flemming@student.moringaschool.com>
2023-09-06 20:37:05 +0300

    Mission Passed

--- app/calculations.py
+++ app/calculations.py
@@ -19,4 +19,11 @@ def calculate_bottle_statistics(session:Session, user_id: int) -> dict:
 
     bottles_recycled = session.query('Bottle').filter_by(user_id=user_id, action='recycled').count()
     bottles_disposed = session.query('Bottle').filter_by(user_id=user_id, action='disposed').count()
-    
+    trash_effect = bottles_recycled - bottles_disposed
+    bottles_statistics = {
+        'bottles_recycled': bottles_recycled ,
+        'bottles_disposed': bottles_disposed,
+        'trash_effect': trash_effect
+    }
+
+    return bottles_statistics
