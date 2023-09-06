9764f54533a54ab3f9390b9ff7f7ee8529f58104
brighton.flemming <brighton.flemming@student.moringaschool.com>
2023-09-06 20:18:20 +0300

    Mission Passed

--- app/calculations.py
+++ app/calculations.py
@@ -5,3 +5,5 @@ from sqlalchemy.orm import Session
 def calculate_tree_statistics(session:Session, user_id: int) -> dict:
 
     trees_planted = session.query(Tree).filter_by(user_id=user_id, action='planted').count()
+    trees_cut_down = session.query(Tree).filter_by(user_id=user_id, action='cut down').count()
+    net_effect = trees_planted - trees_cut_down
